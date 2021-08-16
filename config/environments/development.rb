ad525e7a5bc34535af621a0e23f2355432cb1453
ai-24 <ai.morikawa2410sj@gmail.com>
2021-08-16 21:14:39 +0900

    added password reset function

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
@@ -81,7 +81,7 @@ Rails.application.configure do
     :port => 587,
     :domain => 'smtp.gmail.com',
     :user_name => ENV['GOOGLE_MAIL_ADDRESS'],
-    :password => "0327NKAI",
+    :password => ENV['GOOGLE_MAILER_PASSWORD'],
     :authentication => 'login'
   }
 end
