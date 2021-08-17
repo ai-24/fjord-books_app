d4354d603e1a2830fcd6e5c2a3ef5ea71d3ce4e3
ai-24 <ai.morikawa2410sj@gmail.com>
2021-08-17 22:29:00 +0900

    added users list, function of saving resources and function of editing profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
   def index
+    @users = User.order(:id).page(params[:page])
   end
 
   def show
+    if request.path == '/users/show'
+      @user = current_user
+    else
+      @user = User.find(params[:id])
+    end
   end
+
+  def update
+    @user.update(user_params)
+  end
+
 end
