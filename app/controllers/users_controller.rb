5784b7dcddacdae5fafaa10c937caad119dae919
ai-24 <ai.morikawa2410sj@gmail.com>
2021-08-20 15:50:26 +0900

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def followings
+    @title = "Followings"
+    @user = User.find(params[:id])
+    @users = @user.followings.with_attached_avatar.order(:id).page(params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.with_attached_avatar.order(:id).page(params[:page])
+    render 'show_follow'
+  end
 end
