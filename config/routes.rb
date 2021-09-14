1c037eb63584f190bcedb3cb74b4c23a771e1312
ai-24 <ai.morikawa2410sj@gmail.com>
2021-09-14 11:23:51 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+  resources :follow_lists, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
 end
