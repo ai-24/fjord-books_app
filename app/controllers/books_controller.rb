3794563659d2967a11fd07883a60183f1e669651
ai-24 <ai.morikawa2410sj@gmail.com>
2021-09-28 18:38:54 +0900

    complete rubocop

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,30 +26,30 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
 
-      if @book.save
-        redirect_to @book, notice: t('controllers.common.notice_create', name: Book.model_name.human)
-      else
-        render :new
-      end
+    if @book.save
+      redirect_to @book, notice: t('controllers.common.notice_create', name: Book.model_name.human)
+    else
+      render :new
     end
+  end
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-      if @book.update(book_params)
-        redirect_to @book, notice: t('controllers.common.notice_update', name: Book.model_name.human)
+    if @book.update(book_params)
+      redirect_to @book, notice: t('controllers.common.notice_update', name: Book.model_name.human)
 
-      else
-        render :edit
-      end
+    else
+      render :edit
     end
+  end
 
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
     @book.destroy
-     redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human)
-    end
+    redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human)
+  end
 
   private
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[edit update destroy]
 
@@ -7,9 +9,11 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to @commentable
     else
-      render "/comments/_error_messages"
+      render '/comments/_error_messages'
     end
   end
+
+  def edit; end
 
   def update
     @comment.update(comment_params)
@@ -18,8 +22,8 @@ class CommentsController < ApplicationController
 
   def destroy
     if @comment.user == current_user
-    @comment.destroy
-    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+      @comment.destroy
+      redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
     else
       redirect_to @commentable, alert: t('errors.no_authority')
     end
@@ -30,6 +34,7 @@ class CommentsController < ApplicationController
   def set_comment
     @comment = Comment.find(params[:id])
   end
+
   def comment_params
     params.require(:comment).permit(:comment)
   end
