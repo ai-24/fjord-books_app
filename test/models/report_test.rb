eead23e9e5cc4870fac443f5bb4e0e7374ed16e0
ai-24 <ai.morikawa2410sj@gmail.com>
2021-10-07 11:40:26 +0900

    add report instance method test

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,25 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable?(target_user) should return true when target_user is the user" do
+    user = User.new(email: 'foo@example.com', password: 'password')
+    user.save!
+    user2 = User.new(email: 'hoge@example.com', password: 'password')
+    user2.save!
+
+    report = Report.new(title: 'Rails', content: 'fun to learn',user_id: user.id)
+    report.save!
+    assert report.editable?(user)
+    assert_not report.editable?(user2)
+
+  end
+
+  test "#created_on should return day of week, day month year" do
+    user = User.new(email: 'foo@example.com', password: 'password')
+    user.save!
+    report = Report.new(title: 'Rails', content: 'fun to learn',user_id: user.id)
+    report.save!
+
+    assert_equal Date.today, report.created_on
+  end
 end
